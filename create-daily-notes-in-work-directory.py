0bd534b13b0a0e655e439da0f8416738b30ec2cb
nicrivard <62037628+nicrivard@users.noreply.github.com>
2021-03-20 10:19:13 +0000

    Create create-daily-notes-in-work-directory.py

---
+++ create-daily-notes-in-work-directory.py
@@ -0,0 +1,14 @@
+import os, datetime, pandas as pd
+
+path = 'C:\\Users\\2020745.UK\\OneDrive - EY\Work Vault'
+os.chdir(path)
+
+listOfDates = pd.date_range(start="2021-01-01",end="2021-12-31").tolist()
+
+for date in listOfDates:
+    YYMMDD = date.strftime("%y%m%d")
+    YYYYW = date.strftime("%Y W%W")
+    dailyNoteText = '###### Priori-threes\n1. \n2. \n3. \n\n###### Agenda\n- \n\n###### Actions\n- \n\n```query\nline: ("- [ ]" ' + YYMMDD + ' -"line:")\n```\n\n###### Review\n\n\n###### Completed\n\n```query\nline: ("- [x]" ' + YYMMDD + ' -"line:")\n```\n\n---\nTags: \nReferences: [[' + YYYYW + ']]'
+    title = date.strftime("%y%m%d %A")
+    with open(str(title) + ".md", 'w') as newFile:
+        print(dailyNoteText, file=newFile)
