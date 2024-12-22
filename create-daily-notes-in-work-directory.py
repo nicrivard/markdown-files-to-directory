db3ba016263aa8b5d8d42da67890df86cb8a4d45
nicrivard <62037628+nicrivard@users.noreply.github.com>
2024-12-22 11:00:19 +0000

    Update create-daily-notes-in-work-directory.py

--- create-daily-notes-in-work-directory.py
+++ create-daily-notes-in-work-directory.py
@@ -3,12 +3,12 @@ import os, datetime, pandas as pd
 path = '/Users/nrivard/Work Vault' # 'C:\\Users\\2020745.UK\\OneDrive - EY\Work Vault'
 os.chdir(path)
 
-listOfDates = pd.date_range(start="2021-01-01",end="2021-12-31").tolist()
+listOfDates = pd.date_range(start="2025-01-01",end="2025-12-31").tolist()
 
 for date in listOfDates:
     YYMMDD = date.strftime("%y%m%d")
     YYYYW = date.strftime("%Y W%W")
-    dailyNoteText = '###### Priori-threes\n1. \n2. \n3. \n\n###### Agenda\n- \n\n###### Actions\n- \n\n```query\nline: ("- [ ]" ' + YYMMDD + ' -"line:")\n```\n\n###### Review\n\n\n###### Completed\n\n```query\nline: ("- [x]" ' + YYMMDD + ' -"line:")\n```\n\n---\nTags: \nReferences: [[' + YYYYW + ']]'
+    dailyNoteText = '###### Priori-threes\n1. \n2. \n3. \n\n###### Notes\n- \n\n```query\nline: ("- [ ]" ' + YYMMDD + ' -"line:")\n```\n\n###### Review\n\n\n###### Completed\n\n```query\nline: ("- [x]" ' + YYMMDD + ' -"line:")\n```\n\n---\nTags: \nReferences: [[' + YYYYW + ']]'
     title = date.strftime("%y%m%d %A")
     with open(str(title) + ".md", 'w') as newFile:
         print(dailyNoteText, file=newFile)
