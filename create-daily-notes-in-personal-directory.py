c2d43b78a5747d4cd987b9cab6c98546c7824a69
nicrivard <62037628+nicrivard@users.noreply.github.com>
2026-01-04 07:17:44 +0000

    Update create-daily-notes-in-personal-directory.py
    Updated for 2026

--- create-daily-notes-in-personal-directory.py
+++ create-daily-notes-in-personal-directory.py
@@ -1,9 +1,11 @@
+# pip3 install pandas, datetime
+
 import os, datetime, pandas as pd
 
-path = '/Users/nrivard/Digital Vault'
+path = '/Users/nrivard/Digital Vault/Journal'
 os.chdir(path)
 
-listOfDates = pd.date_range(start="2025-01-01",end="2025-12-31").tolist()
+listOfDates = pd.date_range(start="2026-01-01",end="2026-12-31").tolist()
 
 def suffixGenerator(day):
     if(str(day)[-1] == '1' and (day != '11')):
@@ -21,7 +23,7 @@ for date in listOfDates:
     if(int(day) < 10):
         day = str(day).replace("0", "")
     dailyStoic = date.strftime("%B ") + str(day) + str(suffix)
-    dailyNoteText = "---\ncalories_in: \ncalories_out: \nbmr: 1700\nintermittent_fasting: false\nhuel: false\nvitamins: false\nminerals: false\nprotein: false\nlearning_hour: false\n---\n##### What are you grateful for today? \n\n- \n\n##### What did you learn today? \n\n- \n\n##### What will you improve tomorrow? \
+    dailyNoteText = "---\ncalories_in: \ncalories_out: \nbmr: 1700\nintermittent_fasting: false\nhuel: false\nlearning_hour: false\n---\n##### What are you grateful for today? \n\n- \n\n##### What did you learn today? \n\n- \n\n##### What will you improve tomorrow? \
 \n\n- \n\n---\n![[" + dailyStoic + "]]\n\n---\nTags: #journal "
     title = date.strftime("%y%m%d %A")
     with open(str(title) + ".md", 'w') as newFile:
