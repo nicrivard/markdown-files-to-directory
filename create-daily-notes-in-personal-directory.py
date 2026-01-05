fcb53d2d750e59ad57afbd3d9f667e3163ee255b
nicrivard <62037628+nicrivard@users.noreply.github.com>
2026-01-05 09:43:50 +0000

    Update create-daily-notes-in-personal-directory.py
    Updated to include push-ups and squats checkboxes and block embeds

--- create-daily-notes-in-personal-directory.py
+++ create-daily-notes-in-personal-directory.py
@@ -1,5 +1,3 @@
-# pip3 install pandas, datetime
-
 import os, datetime, pandas as pd
 
 path = '/Users/nrivard/Digital Vault/Journal'
@@ -17,14 +15,51 @@ def suffixGenerator(day):
     else:
         return "th"
 
+def getFitnessBlockId(date):
+    """Generate the block ID for the 365-Day Fitness Challenge embed."""
+    # ISO week number (Jan 5, 2026 = Week 2)
+    week_num = date.isocalendar()[1]
+    # Day of week: Monday=1, Sunday=7
+    day_of_week = date.isoweekday()
+    return f"^d{week_num}a{day_of_week:03d}"
+
 for date in listOfDates:
     day = date.strftime("%d")
     suffix = suffixGenerator(day)
     if(int(day) < 10):
         day = str(day).replace("0", "")
     dailyStoic = date.strftime("%B ") + str(day) + str(suffix)
-    dailyNoteText = "---\ncalories_in: \ncalories_out: \nbmr: 1700\nintermittent_fasting: false\nhuel: false\nlearning_hour: false\n---\n##### What are you grateful for today? \n\n- \n\n##### What did you learn today? \n\n- \n\n##### What will you improve tomorrow? \
-\n\n- \n\n---\n![[" + dailyStoic + "]]\n\n---\nTags: #journal "
+    fitnessBlockId = getFitnessBlockId(date)
+    fitnessEmbed = f"![[365-Day Fitness Challenge#{fitnessBlockId}]]"
+    dailyNoteText = f"""---
+calories_in:
+calories_out:
+bmr: 1700
+intermittent_fasting: false
+huel: false
+learning_hour: false
+push-ups: false
+squats: false
+---
+{fitnessEmbed}
+
+##### What are you grateful for today?
+
+-
+
+##### What did you learn today?
+
+-
+
+##### What will you improve tomorrow?
+
+-
+
+---
+![[{dailyStoic}]]
+
+---
+Tags: #journal """
     title = date.strftime("%y%m%d %A")
     with open(str(title) + ".md", 'w') as newFile:
         print(dailyNoteText, file=newFile)
